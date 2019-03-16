66886d7995b98067be73ec7d7e79034bdb9fd7f5
Alexeyzim <ov.zzimin@gmail.com>
2019-03-16 23:59:04 +0200

    changed script in part for access to site(login.php)

--- scenario1.sh
+++ scenario1.sh
@@ -122,8 +122,8 @@ cat > login.php <<EOF
 define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-global $user;
-$user = user_load(1);
+global \$user;
+\$user = user_load(1);
 drupal_session_regenerate();
 drupal_goto('user');
 ?>
